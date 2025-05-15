c4333d29fd4e9dbcf3df8a5545c2924a03904051
maria cesarotti <mariacesarotti@gmail.com>
2025-05-15 17:11:05 -0300

    fixed .env

--- frontend/src/api/catCalendarApi.tsx
+++ frontend/src/api/catCalendarApi.tsx
@@ -1,4 +1,5 @@
-const API_URL = "https://catagenda-production.up.railway.app";
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 export const getToken = () => {
   const token = localStorage.getItem("token");
